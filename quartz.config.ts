138102515d6c4d8caa536590b3fb8a3db70790bd
kikekrahe <95240884+kikekrahe@users.noreply.github.com>
2025-03-06 22:09:20 +0100

    Quartz sync: Mar 6, 2025, 10:09 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "✶ Graduation EK",
+    pageTitle: "💿 Graduation EK",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
